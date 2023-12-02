081b17bb0ec5cd17be8034cbaa3ba42934e657e5
raquel-castoldi <raquelcastoldi16@gmail.com>
2023-12-01 22:16:34 -0700

    app,jsx

--- TicTacXOs/src/App.jsx
+++ TicTacXOs/src/App.jsx
@@ -1,10 +1,28 @@
-import React from "react";
-import TicTacXOs from "./components/TicTacXOs";
+import React from 'react';
+import { StyleSheet, Text, View, SafeAreaView, StatusBar } from 'react-native';
 
-function App() {
-  return (
-    <div>
-      <TicTacXOs />
-    </div>
-  );
+import HomeScreen from './HomeScreen';
+import EnterName from './EnterName';
+import HowToPlay from './HowToPlay';
+import InGame from './InGame';
+
+export default function App() {
+ return (
+    <SafeAreaView style={styles.container}>
+      <StatusBar barStyle="dark-content" />
+      <HomeScreen />
+      <EnterName />
+      <HowToPlay />
+      <InGame />
+    </SafeAreaView>
+ );
 }
+
+const styles = StyleSheet.create({
+ container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+ },
+});
