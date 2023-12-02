1a527d920c0a0d7a20f254bd5031333d18e19400
Raquel Castoldi <114683499+raquel-castoldi@users.noreply.github.com>
2023-12-01 18:46:57 -0700

    tweaks (#3)

---
+++ src/app.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import TicTacXOs from "./components/TicTacXOs";
+
+function App() {
+  return (
+    <div>
+      <h1>React Tic-Tac-Toe</h1>
+      <TicTacXOs />
+    </div>
+  );
+}
